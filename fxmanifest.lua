1e38b1df7bbcb7fbb2b95b75f0af03eb7b4b2dd3
macids <137509558+macids@users.noreply.github.com>
2023-06-23 22:21:25 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+
+author 'github.com/macids'
+discord 'discord.gg/n4sh'
+description 'Lifting player script. Kucaklama'
+version '1.0'
+
+resource_type 'gametype' {name = 'Macid',}
+client_script 'mc_cl.lua'
